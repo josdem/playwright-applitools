09460e2f5217ef3a73b1f1467c15bfc950b1178a
josdem <joseluis.delacruz@gmail.com>
2023-07-29 19:19:12 -0400

    #2 Changing applitools name

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,25 +1,25 @@
 import { test, expect } from "@playwright/test"
 import properties from "../properties/test.properties"
-import Applitools from "../utils/applitools.util"
+import applitools from "../utils/applitools.util"
 
 test.beforeAll(async () => {
-  Applitools.setUpConfiguration(properties.batchName)
+  applitools.setUpConfiguration(properties.batchName)
 })
 
 test.beforeEach(async ({ page }) => {
-  await Applitools.setUpTest(page, properties.app, test.info().title)
+  await applitools.setUpTest(page, properties.app, test.info().title)
 })
 
 test("should validate page title", async ({ page }) => {
   await page.goto(properties.url)
   await expect(page).toHaveTitle(properties.title)
-  await Applitools.checkWindowEyes("Home Page")
+  await applitools.checkWindowEyes("Home Page")
 })
 
 test.afterEach(async () => {
-  await Applitools.closeEyes()
+  await applitools.closeEyes()
 })
 
 test.afterAll(async () => {
-  await Applitools.cleaning()
+  await applitools.cleaning()
 })
